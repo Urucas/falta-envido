23ca1f5ffbe894930a395dfa93a97617bd9ec091
vrunoa <vruno.alassia@gmail.com>
2016-12-30 12:06:30 -0300

    setting android icon

---
+++ hooks/after_prepare/copy_images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ./res/mipmap-mdpi/icon.png platforms/android/res/mipmap-mdpi
+cp ./res/mipmap-ldpi/icon.png platforms/android/res/mipmap-ldpi
+cp ./res/mipmap-hdpi/icon.png platforms/android/res/mipmap-hdpi
+cp ./res/mipmap-xhdpi/icon.png platforms/android/res/mipmap-xhdpi
+cp ./res/mipmap-xxhdpi/icon.png platforms/android/res/mipmap-xxhdpi
+cp ./res/mipmap-xxxhdpi/icon.png platforms/android/res/mipmap-xxxhdpi
